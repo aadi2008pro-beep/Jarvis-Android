08c9de517328c582beb4ef21bd907f711a6b6583
aadi2008pro-beep <aadi2008pro@gmail.com>
2026-06-01 19:20:00 +0530

    Added JarvisService.kt

---
+++ app/src/main/java/com/jarvis/app/JarvisService.kt
@@ -0,0 +1,18 @@
+package com.jarvis.app
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class JarvisService : AccessibilityService() {
+	    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+	    	        // Jarvis is listening to screen events
+	    	            }
+
+	    	                override fun onInterrupt() {
+	    	                	        // Handle service shutdown
+	    	                	            }
+	    	                	            }
+	    	                	            
+	    	                }
+	    }
+}
