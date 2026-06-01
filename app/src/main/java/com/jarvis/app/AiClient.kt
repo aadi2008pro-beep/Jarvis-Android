887acd78e504192976346984c6ac0c5948c51812
aadi2008pro-beep <aadi2008pro@gmail.com>
2026-06-01 19:37:08 +0530

    Added AiClient network utility

---
+++ app/src/main/java/com/jarvis/app/AiClient.kt
@@ -0,0 +1,16 @@
+package com.jarvis.app
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class AiClient {
+	    private val client = OkHttpClient()
+
+	        // This function will eventually send screen data to the AI
+	            fun askAi(query: String) {
+	            	        // We will add the API URL and processing logic here soon
+	            	            }
+	            	            }
+	            	            
+	            }
+}
