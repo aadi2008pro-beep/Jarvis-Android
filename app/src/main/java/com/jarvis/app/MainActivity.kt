9dcde416f82637761c7c774f5aed356af5ba9640
aadi2008pro-beep <aadi2008pro@gmail.com>
2026-06-01 19:40:19 +0530

    Added UI to activate Jarvis service

--- app/src/main/java/com/jarvis/app/MainActivity.kt
+++ app/src/main/java/com/jarvis/app/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.jarvis.app
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : Activity() {
 	    override fun onCreate(savedInstanceState: Bundle?) {
 	    	        super.onCreate(savedInstanceState)
-	    	                // This is where your UI will live
-	    	                    }
-	    	                    }
-	    	                    
+
+	    	                        val layout = LinearLayout(this)
+	    	                                val button = Button(this)
+	    	                                        button.text = "Activate God Mode"
+
+	    	                                                        button.setOnClickListener {
+	    	                                                        	            // Take the user to Accessibility settings to turn on Jarvis
+	    	                                                        	                        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+	    	                                                        	                                    startActivity(intent)
+	    	                                                        	                                            }
+
+	    	                                                        	                                                            layout.addView(button)
+	    	                                                        	                                                                    setContentView(layout)
+	    	                                                        	                                                                        }
+	    	                                                        	                                                                        }
+	    	                                                        	                                                                        
+	    	                                                        }
 	    }
 }
