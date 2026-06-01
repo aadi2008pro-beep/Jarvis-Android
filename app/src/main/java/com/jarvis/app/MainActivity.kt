ec1a8db847394e29643f6a31a888f8bfe2cec693
aadi2008pro-beep <aadi2008pro@gmail.com>
2026-06-01 19:25:58 +0530

    Added MainActivity.kt

---
+++ app/src/main/java/com/jarvis/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jarvis.app
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+	    override fun onCreate(savedInstanceState: Bundle?) {
+	    	        super.onCreate(savedInstanceState)
+	    	                // This is where your UI will live
+	    	                    }
+	    	                    }
+	    	                    
+	    }
+}
